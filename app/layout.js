2de5a055215f686641fdeb076b1654f721096dcc
DevBasitali <rajaalix2022@gmail.com>
2024-08-16 16:49:22 +0500

    Initial commit from Create Next App

---
+++ app/layout.js
@@ -0,0 +1,15 @@
+
+import "./globals.css";
+
+export const metadata = {
+  title: "Get The Best Prompt ",
+  description: "AI Powered Prompts",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className="">{children}</body>
+    </html>
+  );
+}
