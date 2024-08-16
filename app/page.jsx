0f443aeea1af64a19e1df9d1455b9beba113cc55
DevBasitali <rajaalix2022@gmail.com>
2024-08-16 20:30:01 +0500

    Friday : 8:29

---
+++ app/page.jsx
@@ -0,0 +1,17 @@
+
+
+const Home = () => {
+  return (
+    <section className="w-full flex-col justify-center flex-center">
+        <h1 className="head_text text-center">
+            Share & Discover <br className="max-md:hidden"/>
+            <span className=" orange_gradient text-center">AI Powered Prompts</span>
+        </h1>
+        <p className="desc text-center">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure nulla quo iusto accusantium voluptas. Obcaecati neque ducimus maxime
+        </p>
+    </section>
+  );
+}
+
+export default Home;
